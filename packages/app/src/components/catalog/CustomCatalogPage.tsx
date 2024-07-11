63572b06bfe2498dc3a56c1bc5057fd91375ed49
Marian Steinbach <marian@giantswarm.io>
2024-07-11 15:23:37 +0200

    Restore larger padding in catalog index table

--- packages/app/src/components/catalog/CustomCatalogPage.tsx
+++ packages/app/src/components/catalog/CustomCatalogPage.tsx
@@ -49,7 +49,7 @@ export function CustomCatalogPage(props: CustomCatalogPageProps) {
     actions,
     initiallySelectedFilter = 'owned',
     initialKind = 'component',
-    tableOptions = {},
+    tableOptions = {padding: 'default'},
     emptyContent,
     ownerPickerMode,
   } = props;
