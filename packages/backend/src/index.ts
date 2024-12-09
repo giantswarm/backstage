cebd4049ce2c6bae1acf0a25e9a217f460132240
Dmitry Gusev <gusevda90@gmail.com>
2024-12-09 12:08:22 +0300

    Update Backstage to 1.33.5 (#537)
    * Add Backstage yarn plugin
    
    * Update Backstage packages to v1.33.5
    
    * Add changeset
    
    * Update templates
    
    * Fix ENV variable declaration in Dockerfile
    
    * Copy backstage.json in Dockerfile

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,11 +4,11 @@ import { githubAuthProvider } from './githubAuthProvider';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-app-backend/alpha'));
-backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+backend.add(import('@backstage/plugin-app-backend'));
+backend.add(import('@backstage/plugin-proxy-backend'));
 
 // scaffolder plugin
-backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(
   import('@giantswarm/backstage-plugin-scaffolder-backend-module-gs'),
@@ -20,7 +20,7 @@ backend.add(
 );
 
 // techdocs plugin
-backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
+backend.add(import('@backstage/plugin-techdocs-backend'));
 backend.add(import('@giantswarm/backstage-plugin-techdocs-backend-module-gs'));
 
 // auth plugin
@@ -29,30 +29,30 @@ backend.add(import('@giantswarm/backstage-plugin-auth-backend-module-gs'));
 backend.add(githubAuthProvider);
 
 // events plugin
-backend.add(import('@backstage/plugin-events-backend/alpha'));
+backend.add(import('@backstage/plugin-events-backend'));
 
 // catalog plugin
-backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
-backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
-backend.add(import('@backstage/plugin-catalog-backend-module-aws/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+backend.add(import('@backstage/plugin-catalog-backend-module-aws'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
 // permission plugin
-backend.add(import('@backstage/plugin-permission-backend/alpha'));
+backend.add(import('@backstage/plugin-permission-backend'));
 backend.add(
   import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
 
 // search plugin
-backend.add(import('@backstage/plugin-search-backend/alpha'));
+backend.add(import('@backstage/plugin-search-backend'));
 
 // search collators
-backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+backend.add(import('@backstage/plugin-search-backend-module-catalog'));
+backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes plugin
-backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+backend.add(import('@backstage/plugin-kubernetes-backend'));
 backend.start();
