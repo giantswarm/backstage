0d25e631ff28c97822d71ea9a045f5a886e4ac8a
Dmitry Gusev <gusevda90@gmail.com>
2024-12-18 10:20:37 +0300

    Remove undici proxy (#600)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,27 +1,8 @@
 import 'global-agent/bootstrap';
-import { setGlobalDispatcher, ProxyAgent } from 'undici';
 import { createBackend } from '@backstage/backend-defaults';
 import { BackendFeature } from '@backstage/backend-plugin-api';
 import { githubAuthProvider } from './githubAuthProvider';
 import { rootLogger } from './rootLogger';
-
-const proxyEnv =
-  process.env.GLOBAL_AGENT_HTTP_PROXY || process.env.GLOBAL_AGENT_HTTPS_PROXY;
-
-if (proxyEnv) {
-  const proxyUrl = new URL(proxyEnv);
-  setGlobalDispatcher(
-    new ProxyAgent({
-      uri: proxyUrl.protocol + proxyUrl.host,
-      token:
-        proxyUrl.username && proxyUrl.password
-          ? `Basic ${Buffer.from(
-              `${proxyUrl.username}:${proxyUrl.password}`,
-            ).toString('base64')}`
-          : undefined,
-    }),
-  );
-}
 
 const backend = createBackend();
 
