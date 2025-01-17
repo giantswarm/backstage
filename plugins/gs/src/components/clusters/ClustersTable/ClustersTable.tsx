40299c90dfc0cc4df4e37123064b2c87eee08d41
Marian Steinbach <marian@giantswarm.io>
2025-01-17 12:13:06 +0100

    Add cluster app version column

--- plugins/gs/src/components/clusters/ClustersTable/ClustersTable.tsx
+++ plugins/gs/src/components/clusters/ClustersTable/ClustersTable.tsx
@@ -11,18 +11,19 @@ import SyncIcon from '@material-ui/icons/Sync';
 import { Typography } from '@material-ui/core';
 import type { Cluster, Resource } from '@giantswarm/backstage-plugin-gs-common';
 import {
-  getClusterDescription,
-  getClusterOrganization,
+  getClusterAppVersion,
   getClusterCreationTimestamp,
+  getClusterDescription,
+  getClusterName,
+  getClusterNamespace,
+  getClusterOrganization,
+  getClusterServicePriority,
   isClusterCreating,
   isClusterDeleting,
-  getClusterName,
-  getClusterServicePriority,
-  getClusterNamespace,
 } from '@giantswarm/backstage-plugin-gs-common';
 import { clusterDetailsRouteRef } from '../../../routes';
 import { useClusters } from '../../hooks';
-import { DateComponent } from '../../UI';
+import { DateComponent, Version } from '../../UI';
 import { toSentenceCase } from '../../utils/helpers';
 import { sortAndFilterOptions } from '../../utils/tableHelpers';
 import { ClusterStatus, ClusterStatuses } from '../ClusterStatus';
@@ -51,6 +52,7 @@ type Row = {
   priority?: string;
   status: string;
   apiVersion: string;
+  appVersion: string;
 };
 
 const generatedColumns: TableColumn<Row>[] = [
@@ -84,6 +86,20 @@ const generatedColumns: TableColumn<Row>[] = [
   {
     title: 'Installation',
     field: 'installationName',
+  },
+  {
+    title: 'Cluster App',
+    field: 'appVersion',
+    render: row => {
+      return (
+        <Version
+          version={row.appVersion}
+          sourceLocation="https://github.com/giantswarm/cluster"
+          highlight={true}
+          displayWarning={false}
+        />
+      );
+    },
   },
   {
     title: 'Type',
@@ -138,6 +154,7 @@ const ClustersTableView = ({ loading, retry, resources }: Props) => {
     priority: getClusterServicePriority(resource),
     status: calculateClusterStatus(resource),
     apiVersion: resource.apiVersion,
+    appVersion: getClusterAppVersion(resource),
   }));
 
   return (
