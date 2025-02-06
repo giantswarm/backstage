335ad1e390679e99b8bd9d812ad9b892b7055938
Dmitry Gusev <gusevda90@gmail.com>
2025-02-06 18:18:45 +0300

    Update model types and utility functions

--- plugins/gs-common/src/model/fluxcd/types.ts
+++ plugins/gs-common/src/model/fluxcd/types.ts
@@ -1,3 +1,8 @@
+import * as v1 from './v1';
 import * as v2beta1 from './v2beta1';
 
+export type GitRepository = v1.IGitRepository;
+
 export type HelmRelease = v2beta1.IHelmRelease;
+
+export type Kustomization = v1.IKustomization;
---
+++ plugins/gs-common/src/model/fluxcd/v1/key.ts
@@ -0,0 +1,19 @@
+/**
+ * This file was automatically generated, PLEASE DO NOT MODIFY IT BY HAND.
+ */
+
+export const KustomizationApiVersion = 'kustomize.toolkit.fluxcd.io/v1';
+
+export const KustomizationGVK = {
+  apiVersion: 'v1',
+  group: 'kustomize.toolkit.fluxcd.io',
+  plural: 'kustomizations',
+};
+
+export const GitRepositoryApiVersion = 'source.toolkit.fluxcd.io/v1';
+
+export const GitRepositoryGVK = {
+  apiVersion: 'v1',
+  group: 'source.toolkit.fluxcd.io',
+  plural: 'gitrepositories',
+};
