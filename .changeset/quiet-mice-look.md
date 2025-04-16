03b7e87cbd7a154476378971c05b3ca63428f7f4
Dmitry Gusev <gusevda90@gmail.com>
2025-04-16 13:20:40 +0300

    Use backstage-scaffolder-kubernetes plugin

---
+++ .changeset/quiet-mice-look.md
@@ -0,0 +1,6 @@
+---
+'@giantswarm/backstage-plugin-scaffolder-backend-module-gs': patch
+'backend': patch
+---
+
+Moved from custom scaffolder actions to backstage-scaffolder-kubernetes plugin.
--- plugins/scaffolder-backend-module-gs/src/module.ts
+++ plugins/scaffolder-backend-module-gs/src/module.ts
@@ -1,15 +1,7 @@
-import {
-  coreServices,
-  createBackendModule,
-} from '@backstage/backend-plugin-api';
-import {
-  scaffolderActionsExtensionPoint,
-  scaffolderTemplatingExtensionPoint,
-} from '@backstage/plugin-scaffolder-node/alpha';
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { scaffolderTemplatingExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
 import type { JsonValue } from '@backstage/types';
 import { parseClusterRef } from './filters/parseClusterRef';
-import { kubernetesActions } from './actions';
-import { KubernetesClientFactory } from './lib/kubernetes-client-factory';
 
 export const scaffolderModuleGS = createBackendModule({
   pluginId: 'scaffolder',
@@ -17,25 +9,9 @@ export const scaffolderModuleGS = createBackendModule({
   register(reg) {
     reg.registerInit({
       deps: {
-        actionsExtensionPoint: scaffolderActionsExtensionPoint,
         templatingExtensionPoint: scaffolderTemplatingExtensionPoint,
-        config: coreServices.rootConfig,
-        logger: coreServices.logger,
       },
-      async init({
-        actionsExtensionPoint,
-        templatingExtensionPoint,
-        logger,
-        config,
-      }) {
-        const kubeClientFactory = new KubernetesClientFactory({
-          logger,
-          config,
-        });
-        actionsExtensionPoint.addActions(
-          ...kubernetesActions(kubeClientFactory),
-        );
-
+      async init({ templatingExtensionPoint }) {
         templatingExtensionPoint.addTemplateFilters({
           parseClusterRef: (ref: JsonValue) => parseClusterRef(ref as string),
         });
