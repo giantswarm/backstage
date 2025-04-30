7496927646dca802353223563584072324fafb40
Dmitry Gusev <gusevda90@gmail.com>
2025-04-30 17:12:27 +0300

    Add Cloud Director support

--- plugins/gs/src/components/clusters/cluster-details/ClusterSwitch/ClusterSwitch.tsx
+++ plugins/gs/src/components/clusters/cluster-details/ClusterSwitch/ClusterSwitch.tsx
@@ -3,6 +3,7 @@ import {
   getClusterInfrastructureRef,
   isAWSCluster,
   isAzureCluster,
+  isVCDCluster,
   isVSphereCluster,
 } from '@giantswarm/backstage-plugin-gs-common';
 
@@ -10,12 +11,14 @@ type ClusterSwitchProps = {
   renderAWS: () => React.ReactNode;
   renderAzure: () => React.ReactNode;
   renderVSphere: () => React.ReactNode;
+  renderVCD: () => React.ReactNode;
 };
 
 export const ClusterSwitch = ({
   renderAWS,
   renderAzure,
   renderVSphere,
+  renderVCD,
 }: ClusterSwitchProps) => {
   const { cluster } = useCurrentCluster();
 
@@ -28,6 +31,8 @@ export const ClusterSwitch = ({
       return renderAzure();
     case isVSphereCluster(kind, apiVersion):
       return renderVSphere();
+    case isVCDCluster(kind, apiVersion):
+      return renderVCD();
     default:
       return null;
   }
