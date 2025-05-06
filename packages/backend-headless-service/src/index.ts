aad2bdadf1e356deb4ac72f40c0c40931db7d842
Dmitry Gusev <gusevda90@gmail.com>
2025-05-06 16:24:02 +0300

    Add headless backend

---
+++ packages/backend-headless-service/src/index.ts
@@ -0,0 +1,23 @@
+import 'global-agent/bootstrap';
+import { createBackend } from '@backstage/backend-defaults';
+import { rootLogger } from '@internal/backend-common';
+
+const backend = createBackend();
+
+// auth plugin
+backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@giantswarm/backstage-plugin-auth-backend-module-gs'));
+
+// scaffolder plugin
+backend.add(import('@backstage/plugin-scaffolder-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(
+  import('@giantswarm/backstage-plugin-scaffolder-backend-module-gs'),
+);
+backend.add(import('@aws/aws-core-plugin-for-backstage-scaffolder-actions'));
+backend.add(import('@devangelista/backstage-scaffolder-kubernetes'));
+
+// custom root logger service
+backend.add(rootLogger);
+
+backend.start();
