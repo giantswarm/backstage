86e9a84e58aac9a35ac61a1a82efae63dd7821cc
Dmitry Gusev <gusevda90@gmail.com>
2025-12-18 15:26:19 +0300

    Style fixes

--- plugins/gs/src/components/scaffolder/StepLayout/ValuesDocs.tsx
+++ plugins/gs/src/components/scaffolder/StepLayout/ValuesDocs.tsx
@@ -12,17 +12,18 @@ const darkBackgroundColor = '#333333';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    borderRadius: theme.shape.borderRadius,
-    border: `1px solid ${theme.palette.divider}`,
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? lightBackgroundColor
-        : darkBackgroundColor,
     height: '100%',
-    position: 'relative',
-    overflow: 'hidden',
+
+    [`${theme.breakpoints.up('lg')}`]: {
+      paddingTop: '24px',
+      // paddingBottom: '67px',
+    },
   },
   tabsContainer: {
+    height: '100%',
+    position: 'relative',
+  },
+  tabsContainerInner: {
     position: 'absolute',
     top: 0,
     left: 0,
@@ -30,6 +31,13 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    overflow: 'hidden',
+    borderRadius: theme.shape.borderRadius,
+    border: `1px solid ${theme.palette.divider}`,
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? lightBackgroundColor
+        : darkBackgroundColor,
   },
   tabPanel: {
     '& code': {
@@ -107,45 +115,47 @@ export const ValuesDocs = ({
   return (
     <div className={classes.root}>
       <div className={classes.tabsContainer}>
-        <Box>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            aria-label="Configuration docs"
-          >
-            <Tab label="Defaults" {...a11yProps(0)} />
-            <Tab label="Schema" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        <Box className={classes.codeContainer}>
-          <TabPanel value={value} index={0}>
-            {isLoadingValuesYaml && <Progress />}
-            {!isLoadingValuesYaml && !valuesYaml && (
-              <Typography variant="body1">No values.yaml found</Typography>
-            )}
-            {!isLoadingValuesYaml && valuesYaml && (
-              <CodeSnippet
-                text={valuesYaml}
-                language="yaml"
-                customStyle={{
-                  padding: 0,
-                  background: 'unset',
-                  overflowX: 'unset',
-                }}
-              />
-            )}
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            {isLoadingJsonSchema && <Progress />}
-            {!isLoadingJsonSchema && !jsonSchema && (
-              <Typography variant="body1">No schema found</Typography>
-            )}
-            {!isLoadingJsonSchema && jsonSchema && (
-              <JsonSchemaViewer jsonSchema={jsonSchema} />
-            )}
-          </TabPanel>
-        </Box>
+        <div className={classes.tabsContainerInner}>
+          <Box>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              aria-label="Configuration docs"
+            >
+              <Tab label="Defaults" {...a11yProps(0)} />
+              <Tab label="Schema" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <Box className={classes.codeContainer}>
+            <TabPanel value={value} index={0}>
+              {isLoadingValuesYaml && <Progress />}
+              {!isLoadingValuesYaml && !valuesYaml && (
+                <Typography variant="body1">No values.yaml found</Typography>
+              )}
+              {!isLoadingValuesYaml && valuesYaml && (
+                <CodeSnippet
+                  text={valuesYaml}
+                  language="yaml"
+                  customStyle={{
+                    padding: 0,
+                    background: 'unset',
+                    overflowX: 'unset',
+                  }}
+                />
+              )}
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              {isLoadingJsonSchema && <Progress />}
+              {!isLoadingJsonSchema && !jsonSchema && (
+                <Typography variant="body1">No schema found</Typography>
+              )}
+              {!isLoadingJsonSchema && jsonSchema && (
+                <JsonSchemaViewer jsonSchema={jsonSchema} />
+              )}
+            </TabPanel>
+          </Box>
+        </div>
       </div>
     </div>
   );
