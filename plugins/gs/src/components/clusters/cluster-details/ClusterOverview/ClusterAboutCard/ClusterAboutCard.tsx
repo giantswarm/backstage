39eb4f8eacefa3f8ca1925d22871e4cc32450406
Marian Steinbach <marian@giantswarm.io>
2026-01-06 11:27:19 +0100

    Improve ClusterAboutCard (#1061)
    * Improve ClusterAboutCard
    
    * Add changeset
    
    * Prettier:fix
    
    * Fix ClusterAboutCard styles (#1064)
    
    ---------
    
    Co-authored-by: Dmitry Gusev <gusevda90@gmail.com>

--- plugins/gs/src/components/clusters/cluster-details/ClusterOverview/ClusterAboutCard/ClusterAboutCard.tsx
+++ plugins/gs/src/components/clusters/cluster-details/ClusterOverview/ClusterAboutCard/ClusterAboutCard.tsx
@@ -1,7 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { InfoCard, Link } from '@backstage/core-components';
 import { useRouteRef } from '@backstage/core-plugin-api';
-import { Grid, Tooltip, Typography } from '@material-ui/core';
+import { Box, Grid, Tooltip, Typography } from '@material-ui/core';
 import { Constants } from '@giantswarm/backstage-plugin-gs-common';
 import { AboutField } from '@backstage/plugin-catalog';
 import {
@@ -25,7 +25,6 @@ import {
 } from '../../../../UI';
 import { formatVersion } from '../../../../utils/helpers';
 import { useCurrentCluster } from '../../../ClusterDetailsPage/useCurrentCluster';
-import { ClusterSwitch } from '../../ClusterSwitch';
 import { ProviderClusterLocation } from './ProviderClusterLocation';
 import { clusterDetailsRouteRef } from '../../../../../routes';
 import {
@@ -34,6 +33,69 @@ import {
   useShowErrors,
 } from '@giantswarm/backstage-plugin-kubernetes-react';
 import { getErrorMessage } from '../../../../hooks/utils/helpers';
+import {
+  ClusterTypeManagementIcon,
+  ClusterTypeWorkloadIcon,
+  AWSProviderIcon,
+  AzureProviderIcon,
+} from '../../../../../assets/icons/CustomIcons';
+import { ClusterTypes, ClusterProviders } from '../../../utils';
+
+interface ProviderLocationDisplayProps {
+  provider: string;
+}
+
+function ProviderLocationDisplay({ provider }: ProviderLocationDisplayProps) {
+  const renderProvider = () => {
+    switch (provider) {
+      case ClusterProviders.AWS:
+        return (
+          <Tooltip title="AWS">
+            <Box display="flex">
+              <AWSProviderIcon fontSize="small" />
+            </Box>
+          </Tooltip>
+        );
+      case ClusterProviders.Azure:
+        return (
+          <Tooltip title="Azure">
+            <Box display="flex">
+              <AzureProviderIcon fontSize="small" />
+            </Box>
+          </Tooltip>
+        );
+      default:
+        return (
+          <Typography variant="inherit">
+            {formatClusterProvider(provider)}
+          </Typography>
+        );
+    }
+  };
+
+  const renderLocation = () => {
+    if (
+      provider === ClusterProviders.AWS ||
+      provider === ClusterProviders.Azure
+    ) {
+      return (
+        <Box ml={1}>
+          <Typography variant="inherit">
+            <ProviderClusterLocation />
+          </Typography>
+        </Box>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <Box display="inline-flex" alignItems="center">
+      {renderProvider()}
+      {renderLocation()}
+    </Box>
+  );
+}
 
 export function ClusterAboutCard() {
   const { cluster, installationName } = useCurrentCluster();
@@ -85,13 +147,29 @@ export function ClusterAboutCard() {
   const k8sVersion = controlPlane ? controlPlane.getK8sVersion() : undefined;
 
   return (
-    <InfoCard title="About">
+    <InfoCard>
       <Grid container spacing={5}>
-        <AboutField
-          label="Type"
-          value={formatClusterType(clusterType)}
-          gridSizes={{ xs: 6, md: 4 }}
-        />
+        <AboutField label="Type" gridSizes={{ xs: 6, md: 4 }}>
+          <AboutFieldValue>
+            <Box display="inline-flex" alignItems="center">
+              {clusterType === ClusterTypes.Management ? (
+                <Tooltip title="Management cluster">
+                  <Box display="flex" mr={1}>
+                    <ClusterTypeManagementIcon />
+                  </Box>
+                </Tooltip>
+              ) : (
+                <Tooltip title="Workload cluster">
+                  <Box display="flex" mr={1}>
+                    <ClusterTypeWorkloadIcon />
+                  </Box>
+                </Tooltip>
+              )}
+              <span>{formatClusterType(clusterType)}</span>
+            </Box>
+          </AboutFieldValue>
+        </AboutField>
+
         <AboutField label="Kubernetes version" gridSizes={{ xs: 6, md: 4 }}>
           <AboutFieldValue>
             <AsyncValue
@@ -103,47 +181,20 @@ export function ClusterAboutCard() {
               {value => (
                 <KubernetesVersion
                   version={formatVersion(value)}
-                  hideIcon
+                  hideIcon={false}
                   hideLabel
                 />
               )}
             </AsyncValue>
           </AboutFieldValue>
         </AboutField>
+
         <AboutField label="Release" gridSizes={{ xs: 6, md: 4 }}>
           <AboutFieldValue>
             {releaseVersion ? formatVersion(releaseVersion) : <NotAvailable />}
           </AboutFieldValue>
         </AboutField>
-        <AboutField
-          label="Organization"
-          value={organization}
-          gridSizes={{ xs: 6, md: 4 }}
-        />
-        <AboutField label="Service priority" gridSizes={{ xs: 6, md: 4 }}>
-          <AboutFieldValue>
-            {servicePriority ? (
-              formatServicePriority(servicePriority)
-            ) : (
-              <NotAvailable />
-            )}
-          </AboutFieldValue>
-        </AboutField>
-        <AboutField label="Provider" gridSizes={{ xs: 6, md: 4 }}>
-          <AboutFieldValue>
-            {provider ? formatClusterProvider(provider) : <NotAvailable />}
-          </AboutFieldValue>
-        </AboutField>
-        <AboutField label="Region" gridSizes={{ xs: 6, md: 4 }}>
-          <AboutFieldValue>
-            <ClusterSwitch
-              renderAWS={() => <ProviderClusterLocation />}
-              renderAzure={() => <ProviderClusterLocation />}
-              renderVSphere={() => <NotAvailable />}
-              renderVCD={() => <NotAvailable />}
-            />
-          </AboutFieldValue>
-        </AboutField>
+
         {!isManagementCluster(cluster) && (
           <AboutField
             label="Installation"
@@ -166,6 +217,41 @@ export function ClusterAboutCard() {
             </AboutFieldValue>
           </AboutField>
         )}
+
+        <AboutField
+          label={
+            provider === ClusterProviders.AWS ||
+            provider === ClusterProviders.Azure
+              ? 'Provider/Location'
+              : 'Provider'
+          }
+          gridSizes={{ xs: 6, md: 4 }}
+        >
+          <AboutFieldValue>
+            {provider ? (
+              <ProviderLocationDisplay provider={provider} />
+            ) : (
+              <NotAvailable />
+            )}
+          </AboutFieldValue>
+        </AboutField>
+
+        <AboutField
+          label="Organization"
+          value={organization}
+          gridSizes={{ xs: 6, md: 4 }}
+        />
+
+        <AboutField label="Service priority" gridSizes={{ xs: 6, md: 4 }}>
+          <AboutFieldValue>
+            {servicePriority ? (
+              formatServicePriority(servicePriority)
+            ) : (
+              <NotAvailable />
+            )}
+          </AboutFieldValue>
+        </AboutField>
+
         <AboutField label="Created" gridSizes={{ xs: 6, md: 4 }}>
           <AboutFieldValue>
             <Typography variant="inherit">
