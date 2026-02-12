9521e9a7378b1363007b72774d545b6a4b44cad3
Dmitry Gusev <gusevda90@gmail.com>
2026-02-12 12:17:29 +0300

    Move ErrorStatus component from gs plugin to ui-react

---
+++ .changeset/keen-meadow-drift.md
@@ -0,0 +1,5 @@
+---
+'@giantswarm/backstage-plugin-ui-react': patch
+---
+
+Move ErrorStatus component from gs plugin to ui-react.
--- plugins/ui-react/src/components/index.ts
+++ plugins/ui-react/src/components/index.ts
@@ -4,6 +4,7 @@ export * from './AsyncValue';
 export * from './Autocomplete';
 export * from './ContentRow';
 export * from './DateComponent';
+export * from './ErrorStatus';
 export * from './ExternalLink';
 export * from './MultiplePicker';
 export * from './MultipleSelect';
