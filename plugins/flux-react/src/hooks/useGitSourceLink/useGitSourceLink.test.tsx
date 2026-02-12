e3947d2c6033b551965658490966e9f95729f6bc
Dmitry Gusev <gusevda90@gmail.com>
2026-02-12 18:58:04 +0300

    Add tests for useGitSourceLink hook in flux-react

---
+++ plugins/flux-react/src/hooks/useGitSourceLink/useGitSourceLink.test.tsx
@@ -0,0 +1,202 @@
+import { configApiRef } from '@backstage/core-plugin-api';
+import { ApiProvider } from '@backstage/core-app-api';
+import { mockApis, TestApiRegistry } from '@backstage/test-utils';
+import { renderHook } from '@testing-library/react';
+import { useGitSourceLink } from './useGitSourceLink';
+
+const testDefaultBehaviour = (renderHookFn: any) => {
+  it('should return undefined if url and revision are missing', () => {
+    const { result: result1 } = renderHookFn({});
+    expect(result1.current).toBeUndefined();
+
+    const { result: result2 } = renderHookFn({
+      url: 'https://example.com',
+    });
+    expect(result2.current).toBeUndefined();
+
+    const { result: result3 } = renderHookFn({
+      revision: '1234567890',
+    });
+    expect(result3.current).toBeUndefined();
+  });
+
+  it('should return URL without path when path is not provided', () => {
+    const url = 'https://github.example.com/test-project/test-repo';
+    const revision = '1234567890';
+
+    const { result } = renderHookFn({ url, revision });
+
+    expect(result.current).toBe(
+      'https://github.example.com/test-project/test-repo/tree/1234567890',
+    );
+  });
+
+  it('should return correct URL for GitHub (SSH)', () => {
+    const url1 = 'ssh://git@github.example.com:443/test-project/test-repo.git';
+    const url2 = 'ssh://git@github.example.com/test-project/test-repo';
+    const revision = '1234567890';
+    const path = './test/repo/path';
+
+    const { result: result1 } = renderHookFn({
+      url: url1,
+      revision,
+      path,
+    });
+
+    expect(result1.current).toEqual(
+      'https://github.example.com/test-project/test-repo/tree/1234567890/test/repo/path',
+    );
+
+    const { result: result2 } = renderHookFn({
+      url: url2,
+      revision,
+      path,
+    });
+
+    expect(result2.current).toEqual(
+      'https://github.example.com/test-project/test-repo/tree/1234567890/test/repo/path',
+    );
+  });
+
+  it('should return correct URL for GitHub (HTTPS)', () => {
+    const url = 'https://github.example.com/test-project/test-repo';
+    const revision = '1234567890';
+    const path = './test/repo/path';
+
+    const { result } = renderHookFn({ url, revision, path });
+
+    expect(result.current).toBe(
+      'https://github.example.com/test-project/test-repo/tree/1234567890/test/repo/path',
+    );
+  });
+
+  it('should correctly parse branch@sha1:commit revision format', () => {
+    const url = 'https://github.example.com/test-project/test-repo';
+    const revision = 'main@sha1:abc123def456';
+    const path = './test/repo/path';
+
+    const { result } = renderHookFn({ url, revision, path });
+
+    expect(result.current).toBe(
+      'https://github.example.com/test-project/test-repo/tree/abc123def456/test/repo/path',
+    );
+  });
+
+  it('should return undefined if url does not match any known patterns', () => {
+    const url = 'https://example.net/test-project/test-repo.git';
+    const revision = '1234567890';
+    const path = './test/repo/path';
+
+    const { result } = renderHookFn({ url, revision, path });
+
+    expect(result.current).toBeUndefined();
+  });
+};
+
+describe('useGitSourceLink', () => {
+  describe('when configuration for Git repository URL patterns is not provided', () => {
+    const configApiMock = mockApis.config({
+      data: {},
+    });
+
+    const apis = TestApiRegistry.from([configApiRef, configApiMock]);
+    const wrapper = ({ children }: { children?: any }) => {
+      return <ApiProvider apis={apis}>{children}</ApiProvider>;
+    };
+
+    const renderHookWithWrapper = ({
+      url,
+      revision,
+      path,
+    }: {
+      url?: string;
+      revision?: string;
+      path?: string;
+    }) => {
+      return renderHook(
+        () => {
+          return useGitSourceLink({ url, revision, path });
+        },
+        {
+          wrapper,
+        },
+      );
+    };
+
+    testDefaultBehaviour(renderHookWithWrapper);
+  });
+
+  describe('when configuration for Git repository URL patterns is provided', () => {
+    const configApiMock = mockApis.config({
+      data: {
+        flux: {
+          gitRepositoryPatterns: [
+            {
+              targetUrl:
+                'https://${{HOSTNAME}}/projects/${{PROJECT_NAME}}/repos/${{REPOSITORY_NAME}}/browse/${{PATH}}?at=${{REVISION}}',
+              gitRepositoryUrlPattern:
+                '^https:\\/\\/(?<HOSTNAME>bitbucket.+?)\\/scm\\/(?<PROJECT_NAME>.+?)\\/(?<REPOSITORY_NAME>.+?)(\\.git)?$',
+            },
+            {
+              targetUrl:
+                'https://${{HOSTNAME}}/${{REPOSITORY_PATH}}/-/tree/${{REVISION}}/${{PATH}}',
+              gitRepositoryUrlPattern:
+                '^ssh:\\/\\/git@(?<HOSTNAME>gitlab.+?)\\/(?<REPOSITORY_PATH>.+?)(\\.git)?$',
+            },
+          ],
+        },
+      },
+    });
+
+    const apis = TestApiRegistry.from([configApiRef, configApiMock]);
+    const wrapper = ({ children }: { children?: any }) => {
+      return <ApiProvider apis={apis}>{children}</ApiProvider>;
+    };
+
+    const renderHookWithWrapper = ({
+      url,
+      revision,
+      path,
+    }: {
+      url?: string;
+      revision?: string;
+      path?: string;
+    }) => {
+      return renderHook(
+        () => {
+          return useGitSourceLink({ url, revision, path });
+        },
+        {
+          wrapper,
+        },
+      );
+    };
+
+    testDefaultBehaviour(renderHookWithWrapper);
+
+    it('should return correct URL for BitBucket', () => {
+      const url =
+        'https://bitbucket.example.net/scm/test-project/test-repo.git';
+      const revision = '1234567890';
+      const path = './test/repo/path';
+
+      const { result } = renderHookWithWrapper({ url, revision, path });
+
+      expect(result.current).toEqual(
+        'https://bitbucket.example.net/projects/test-project/repos/test-repo/browse/test/repo/path?at=1234567890',
+      );
+    });
+
+    it('should return correct URL for GitLab', () => {
+      const url = 'ssh://git@gitlab.example.com/test-project/test-repo.git';
+      const revision = '1234567890';
+      const path = './test/repo/path';
+
+      const { result } = renderHookWithWrapper({ url, revision, path });
+
+      expect(result.current).toEqual(
+        'https://gitlab.example.com/test-project/test-repo/-/tree/1234567890/test/repo/path',
+      );
+    });
+  });
+});
