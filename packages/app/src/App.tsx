7ed848cbdb24e81ffef3810dea25e16ca1e965fb
Timo Derstappen <teemow@gmail.com>
2026-09-04 22:35:48 +0200

    Revert "feat(github-actions): the catalog's GitHub Actions tab as the signed-in person through muster (#2274)" (#2275)
    This reverts commit 1d76ab644ee09903297188329431abe801cd6611.

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -36,7 +36,6 @@ import { kubernetesPluginOverrides } from './modules/kubernetes';
 import apiDocsPlugin from '@backstage/plugin-api-docs/alpha';
 import { apiDocsPluginOverrides } from './modules/api-docs';
 import githubActionsPlugin from '@backstage-community/plugin-github-actions/alpha';
-import { githubActionsPluginOverrides } from './modules/github-actions';
 import pagerDutyPlugin from '@pagerduty/backstage-plugin/alpha';
 import signalsPlugin from '@backstage/plugin-signals/alpha';
 
@@ -91,7 +90,6 @@ const app = createApp({
     apiDocsPlugin,
     apiDocsPluginOverrides,
     githubActionsPlugin,
-    githubActionsPluginOverrides,
     pagerDutyPlugin,
     signalsPlugin,
 
